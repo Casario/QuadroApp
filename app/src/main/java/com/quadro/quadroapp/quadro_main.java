f72089e06d67e12ff6d1c333b1b2ef0b6038042c
Daniel <dani.rapp@gmx.de>
2018-06-12 21:56:14 +0200

    V1.1

--- app/src/main/java/com/quadro/quadroapp/quadro_main.java
+++ app/src/main/java/com/quadro/quadroapp/quadro_main.java
@@ -2,6 +2,7 @@ package com.quadro.quadroapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,14 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class quadro_main extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
+    TextView textView; //hier
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,6 @@ public class quadro_main extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         bluetoothConnection.bluetoothSetup(this, REQUEST_ENABLE_BT);
-
 
         Button send = (Button) findViewById(R.id.button_send);
         Button connect = (Button) findViewById(R.id.button_connect);
@@ -45,11 +48,26 @@ public class quadro_main extends AppCompatActivity {
         });
 
 
+        TextView statuswindow = (TextView)findViewById(R.id.textView); //hier
+        powerSwitch.setOnCheckedChangeListener(this);
+
+        @Override
+
+            if(powerSwitch.isChecked()){
+                textView.setText("Switch ON");
+            }else{
+                textView.setText("Switch OFF");
+            }
+
+
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "t15035@hb.dhbw-stuttgart.de\nt15043@hb.dhbw-stuttgart.de", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Daniel Rapp\t\t\tt15035@hb.dhbw-stuttgart.de\nKevin Sieg\t\t\t\tt15043@hb.dhbw-stuttgart.de", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
